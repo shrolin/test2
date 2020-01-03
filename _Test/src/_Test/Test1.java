c0a1c0a43cfb0945e33c14a5ae59bcd12f9c5493
000 <000@192.168.21.50>
2020-01-03 11:56:42 +0800

    111

--- _Test/src/_Test/Test1.java
+++ _Test/src/_Test/Test1.java
@@ -1,9 +1,13 @@
 package _Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test1 {
 
 	int a=1;
 
   String s= "abc";
-
+  	private Date date;
+  	private Calendar calender;
 }
